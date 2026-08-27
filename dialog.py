246414dd4c44e161c34125559c8e266fc572a9f9
William_Lind <willin35@edu.umea.se>
2026-08-27 13:31:02 +0200

    kalkylator 28/8

--- dialog.py
+++ dialog.py
@@ -2,4 +2,4 @@ namn = input("Vad heter du?")
 ålder = int(input("Hur gammal är du?"))
 
 print (f"hej {namn} !")
-print (f"du är {ålder} gammal")
+print (f"du är {ålder} år gammal")
---
+++ kalkylator.py
@@ -0,0 +1,4 @@
+talett = float(input("skriv det första talet "))
+taltvå = float(input("skriv det andra talet "))
+
+print (f"svaret är = {talett + taltvå}")
