f0bdec742757197b0c2b68c9894c6c440194fc08
William_Lind <willin35@edu.umea.se>
2026-09-01 11:32:11 +0200

    tisdag 1 septebert

---
+++ gissar.py
@@ -0,0 +1,14 @@
+import random
+
+datorn = random.randint(1,20)
+människa = random.randint(1,20)
+
+print (f"du slog {människa}")
+print (f"datorns slog {datorn}")
+
+if människa > datorn:
+    print ("du slog roboten")
+elif människa == datorn:
+    print ("ni båda förlorade")
+elif människa < datorn:
+    print ("du förlorade")
